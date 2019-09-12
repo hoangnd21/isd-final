f6101e33259cf3672979b64db9bf12bea31fc5f5
hoangnd21 <hoangnd2112@gmail.com>
2019-09-12 14:31:52 +0700

    init

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BasicLayout from './BasicLayout'
+
+ReactDOM.render(<BasicLayout />, document.getElementById('root'));
+
